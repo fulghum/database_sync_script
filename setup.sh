ed57c2d3f61b69cf0bec3582030ccf4fd37f9c62
Jason Fulghum <jason@dolthub.com>
2022-05-10 09:34:25 -0700

    Adding initial files

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# MariaDB Source Database
+export SHARED_DB_HOST=localhost
+export SHARED_DB_HOST_PORT=3307
+export SHARED_DB_USER=root
+export SHARED_DB_PASSWORD=
+
+# Dolt Destination Database
+export MYSQL_HOST=localhost
+export MYSQL_PORT=3306
+export MYSQL_USER=root
+export MYSQL_PASSWORD=
